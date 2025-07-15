a21d7aa11098a50f942aaa2e6a92558314d23448
gratudy <gratudy@gmail.com>
2025-07-14 21:35:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const cursos = [
+  {
+    id: "MAT001",
+    nombre: "Álgebra matricial y geometría analítica",
+    ciclo: 1,
+    prerrequisitos: [],
+    desbloquea: ["CAL001", "CAL002"]
+  },
+  ...
+];
 
